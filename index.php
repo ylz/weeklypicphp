0cc5d20ceed64393f403ab7c2286fb178a393f56
Dan Mandowar Lx <ylz@ylz.ch>
2019-07-21 15:21:43 +0200

    add htaccess

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Titel</title>
+    <title>.:fotoaufbrecherli:.</title>
   </head>
   <body>
   	<?php
