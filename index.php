ee5c4a0872e39f19718bfc10723b7c499bfa4e41
Dan Mandowar Lx <ylz@ylz.ch>
2019-07-21 14:45:44 +0200

    add index file structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Titel</title>
+  </head>
+  <body>
+  	<?php
+  		//####################################################################
+      echo "Hallo! <3";
+    ?>
+
+
+
+    <?php
+      //####################################################################
+  	?>
+  </body>
+</html>
