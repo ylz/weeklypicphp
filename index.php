1bc7ab70dc7b5d052bc7c7fb206727fd5c372d91
Dan Mandowar Lx <ylz@ylz.ch>
2019-07-21 15:45:20 +0200

    Einführungstext

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
   <body>
   	<?php
   		//####################################################################
-      echo "Hallo! <3";
+      echo "<h1>Hallo! <3</h1>";
+      echo "<p>Hier kannst du einfach dein Wochen- oder Monatsbild skalieren auf 2048 Pixel (lange Kante) und deine EXIF Beschreibung als Tag setzen. Das Ergebnis lädst du auf dein Gerät wieder herunter und danach auf <a href='https://upload.weeklypic.de/'>https://upload.weeklypic.de/</a> hochladen</p>"
+      echo "<p>Disclaimer: Ja, die Bilder werden zumindest aktuell noch auf diesem Server hier gespeichert und sind mit dem selbem Passwort geschützt abrufbar. Ich lösche die Bilder periodisch von Hand.</p>"
     ?>
 
 
