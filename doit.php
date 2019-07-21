bffb9c6eafbee141af8f98c7304424375a36cd6b
Dan Mandowar Lx <ylz@ylz.ch>
2019-07-21 21:40:13 +0200

    doit start

---
+++ doit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>.:fotoaufbrecherli:.</title>
+  </head>
+  <body>
+    <?php
+      //####################################################################
+      // _POST Var Handling
+      $user = $_POST["user"];
+      $description = $_POST["description"];
+      //####################################################################
+      echo "<h1>Hallo! <3</h1>";
+      echo "<p>Grüezi $user. Dein Bild soll also $description heissen?</p>";
+
+    ?>
+
+
+
+
+    <?php
+      //####################################################################
+    ?>
+  </body>
+</html>
