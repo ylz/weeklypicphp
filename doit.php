3d51bb1db9949ab21af1c123d6cf8fb95a3f135c
Dan Mandowar Lx <ylz@ylz.ch>
2019-07-22 16:02:05 +0200

    update the latest and greatest

--- doit.php
+++ doit.php
@@ -11,10 +11,62 @@
       // _POST Var Handling
       $user = $_POST["user"];
       $description = $_POST["description"];
-      //####################################################################
+      $description_isset = false;
       echo "<h1>Hallo! <3</h1>";
-      echo "<p>Grüezi $user. Dein Bild soll also $description heissen?</p>";
+      echo "<p>Grüezi $user.</p>";
+      if (!empty($description))
+      {
+        echo "<p>Dein Bild soll also **$description** heissen?</p>";  
+        $description_isset = true;
+      }
+      //####################################################################
+      // File Upload Handling
+      $upload_folder = '_files/'; //Das Upload-Verzeichnis
+      $filename = pathinfo($_FILES['datei']['name'], PATHINFO_FILENAME);
+      $extension = strtolower(pathinfo($_FILES['datei']['name'], PATHINFO_EXTENSION));
+       
+       
+      //Überprüfung der Dateiendung
+      $allowed_extensions = array('png', 'jpg', 'jpeg', 'JPG', 'JPEG');
+      if(!in_array($extension, $allowed_extensions)) {
+       die("Ungültige Dateiendung.");
+      }
+       
+      //Überprüfung der Dateigröße
+      $max_size = 5000*1024; //5MB
+      if($_FILES['datei']['size'] > $max_size) {
+       die("Bitte keine Dateien größer 5MB hochladen");
+      }
+       
+      //Überprüfung dass das Bild keine Fehler enthält
+      if(function_exists('exif_imagetype')) { //Die exif_imagetype-Funktion erfordert die exif-Erweiterung auf dem Server
+       $allowed_types = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+       $detected_type = exif_imagetype($_FILES['datei']['tmp_name']);
+       if(!in_array($detected_type, $allowed_types)) {
+       die("Nur der Upload von Bilddateien ist gestattet");
+       }
+      }
+      else {
+        die("No PHP-EXIF functions");
+      } 
 
+      //Pfad zum Upload
+      $new_path = $upload_folder.$filename.'.'.$extension;
+       
+      //Neuer Dateiname falls die Datei bereits existiert
+      if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
+       $id = 1;
+       do {
+       $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
+       $id++;
+       } while(file_exists($new_path));
+      }
+       
+      //Alles okay, verschiebe Datei an neuen Pfad
+      move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
+      echo 'Bild erfolgreich hochgeladen: <a href="'.$new_path.'">'.$new_path.'</a>';
+
+      
     ?>
 
 
--- index.php
+++ index.php
@@ -10,23 +10,23 @@
   		//####################################################################
       echo "<h1>Hallo! <3</h1>";
       echo "<p>Hier kannst du einfach dein Wochen- oder Monatsbild skalieren auf 2048 Pixel (lange Kante) und deine EXIF Beschreibung als Tag setzen. Das Ergebnis lädst du auf dein Gerät wieder herunter und danach auf <a href='https://upload.weeklypic.de/'>https://upload.weeklypic.de/</a> hoch.</p>";
-      echo "<p>Disclaimer: Ja, die Bilder werden zumindest aktuell noch auf diesem Server hier gespeichert und sind mit dem selbem Passwort geschützt abrufbar. Ich lösche die Bilder periodisch von Hand.</p>";
+      echo "<p>Disclaimer: Ja, die Bilder werden zumindest aktuell noch auf diesem Server hier gespeichert und sind mit dem selbem Passwort geschützt abrufbar. Ich lösche die Bilder periodisch von Hand. Für die Funktionalität der Script und auch die Verfügbarkeit übernehme ich weder Garantie noch Haftung.</p>";
     ?>
 
+    <p>
+      <form action="doit.php" method="post" enctype="multipart/form-data">
+        Datei auswählen:
+        <input type="file" name="fileToUpload" id="fileToUpload" required>
+        <br/>
+        Benutzername:
+        <input type="text" id="user" name="user" required>
+        <br/>
+        Bildbeschreibung (optional):
+        <input type="text" id="description" name="description">
+        <br/>
+        <input type="submit" value="Upload Image" name="submit">
+      </form>
+  </p>
 
-    <form action="doit.php" method="post" enctype="multipart/form-data">
-      Datei auswählen:
-      <input type="file" name="fileToUpload" id="fileToUpload" required>
-      Benutzername:
-      <input type="text" id="user" name="user" required>
-      Bildbeschreibung (optional):
-      <input type="text" id="description" name="description">
-      <input type="submit" value="Upload Image" name="submit">
-    </form>
-
-
-    <?php
-      //####################################################################
-  	?>
   </body>
 </html>
