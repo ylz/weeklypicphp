1e2ee87922b480f69bb395e2cc9ad518627f7c43
Alexander K <a-github@alice-and-bob.de>
2019-08-01 01:26:37 +0200

    resizing works
    also startet input parameters to generate WeeklyPic special filenames.

--- doit.php
+++ doit.php
@@ -19,12 +19,24 @@
   <body>
 
     <?php
+
       $debugging = true;
+      $convert_command = '/usr/local/bin/convert';  // imagemagick covert
+
       include 'src/exif_parsing.php';
+
+      // TODO: replace "die" with function and flag to keep HTML result clean and offer a link back.
+      // IDEA: generate filename from parameters (week/month, w/m-number, Weekly-Pic-Name)
+      // IDEA: validate picture date against parameters week/month and w/m-number
 
       //####################################################################
       // _POST Var Handling
+
+      // TODO: Harden _POST values (Restrict to certain characters)
+
       $user = $_POST["user"];
+      $creator = $_POST["creator"];
+      $license = $_POST["license"];
       $description = $_POST["description"];
       $description_isset = false;
       echo "<h1>Hallo! ❤️</h1>";
@@ -58,9 +70,9 @@
       }
 
       //Überprüfung der Dateigröße
-      $max_size = 5000*1024; //5MB
+      $max_size = 10000*1024; //10MB
       if($_FILES['fileToUpload']['size'] > $max_size) {
-        die("Bitte keine Dateien größer 5MB hochladen");
+        die("Bitte keine Dateien größer 10 MB hochladen");
       }
 
       //Überprüfung dass das Bild keine Fehler enthält
@@ -80,14 +92,17 @@
         die("No PHP-EXIF functions");
       }
 
-      // Pfad zum Upload // TODO: mit generiertem Dateinamen?
+      // Pfad zum Upload
+      // IDEA: Oder Name generiertem Dateinamen erstellen?
       $new_path = $upload_folder.$filename.'.'.$extension;
 
-      //Neuer Dateiname falls die Datei bereits existiert  // TODO: oder bestehendes Bild überschreiben
+      //Neuer Dateiname falls die Datei bereits existiert
+      // IDEA: Oder bestehendes Bild überschreiben?
       if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
         $id = 1;
         do {
           $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
+          $tmp_file = $upload_folder.$filename.'_'.$id.'_tmp.'.$extension;
           $id++;
         } while(file_exists($new_path));
       }
@@ -99,33 +114,67 @@
       //####################################################################
       // generate requestet EXIF values
 
-      // TODO: $requested['FileName'] =
+      $requested['.FileName']              = '';
+
       $requested['Title']                  = $description;
+      $requested['ObjectName']             = $requested['Title'];
+
       $requested['ImageDescription']       = $user . ' / ' . $description;
       $requested['Description']            = $requested['ImageDescription'];
-      $requested['ImageWidth']             = '2000';
-      $requested['ImageHeight']            = '2000';
+      $requested['Caption-Abstract']       = $requested['ImageDescription'];
+
+      $requested['.ImageWidth']            = '2000';
+      $requested['.ImageHeight']           = '2000';
       $requested['ExifImageWidth']         = $requested['ImageWidth'];
       $requested['ExifImageHeight']        = $requested['ImageHeight'];
-      $requested['Artist']                 = $_POST["creator"];
+
+      $requested['Artist']                 = $creator;
       $requested['Creator']                = $requested['Artist'];
-      $requested['Copyright']              = $_POST["license"];
+      $requested['By-line']                = $requested['Artist'];
+
+      $requested['Copyright']              = $license;
       $requested['Rights']                 = $requested['Copyright'];
-      $requested['.Geo']                   = 'Nein';
+      $requested['CopyrightNotice']        = $requested['Copyright'];
+      // $requested['ProfileCopyright']       = ''; // not user specific
+
+      $requested['URL']                    = '';
+      $requested['WebStatement']           = $requested['URL'];
+      $requested['CreatorWorkURL']         = $requested['URL'];
+
+      $requested['?GPS']                   = 'Nein';
 
       //####################################################################
       // display picture attributes (EXIF) existing compared to requested
 
       echo '<h2>Eckdaten des <i>hochgeladenen</i> Bildes</h2>';
-      $fixed_size = exif_display($new_path, $requested); 
+      exif_display($new_path, $requested);
 
       //####################################################################
       // resize picture
 
+      $command =  $convert_command . ' ' . escapeshellarg($new_path) .
+                  ' -resize 2000x2000 ' . escapeshellarg($tmp_file);
+      exec($command, $data, $result);
+      if($debugging) { // debug
+        echo "<p>command: "; print_r($command);
+        echo "<br>data: <br><pre>"; print_r($data); echo "</pre>";
+        echo "<br>result: "; print_r($result);
+        echo "</p>";
+      }
+      if($result !== 0) {
+        die('Fehler bei der Größenänderung.');
+      }
+      if(unlink($new_path) == false) {
+        die('Fehler beim Löschen der alten Datei. (resize)');
+      }
+      if(rename($tmp_file, $new_path) == false) {
+        die('Fehler beim Umbennen der temporärern Datei. (resize)');
+      }
 
       //####################################################################
       // update picture EXIF to requested/required attributes
 
+      // TODO: implement retagging
 
       //####################################################################
       // display picture attributes (EXIF) existing compared to requested
@@ -135,9 +184,20 @@
 
       //####################################################################
       // display picture  and  furhter actions (buttons) to delete picture
-      // maybe directly send them to tim peters owncloud?
+      // IDEA: maybe directly send them to tim peters owncloud?
 
+      echo '<h2>Das überarbeitete Bild! </h2>';
+      echo '<p><img src="' . $new_path . '" alt="Your processed WeeklyPic" width="600"></p> ';
+
+      // TODO: implement deletion
 
     ?>
+
+    <h2>Und nun?</h2>
+    <p>Nachdem du das Bild (mit einem Rechtsklick auf das Bild)
+       heruntergeladen hast, lädst du es wieder auf
+       <a href='https://upload.weeklypic.de/'>https://upload.weeklypic.de/</a> hoch. </p>
+    <p><a href="index.php">Hier geht es dann an den Anfang zurück.</a></p>
+
   </body>
 </html>
--- index.php
+++ index.php
@@ -15,11 +15,21 @@
     </style>
   </head>
   <body>
-  	<?php
-  		//####################################################################
-      echo "<h1>Hallo! ❤️</h1>";
-      echo "<p>Hier kannst du einfach dein Wochen- oder Monatsbild skalieren auf 2048 Pixel (lange Kante) und deine EXIF Beschreibung als Tag setzen. Das Ergebnis lädst du auf dein Gerät wieder herunter und danach auf <a href='https://upload.weeklypic.de/'>https://upload.weeklypic.de/</a> hoch.</p>";
-      echo "<p>Disclaimer: Ja, die Bilder werden zumindest aktuell noch auf diesem Server hier gespeichert und sind mit dem selbem Passwort geschützt abrufbar. Ich lösche die Bilder periodisch von Hand. Für die Funktionalität der Script und auch die Verfügbarkeit übernehme ich weder Garantie noch Haftung.</p>";
+
+  	<h1>Hallo! ❤️</h1>
+    <p>Hier kannst du einfach dein Wochen- oder Monatsbild skalieren auf
+       2048 Pixel (lange Kante) und deine EXIF Beschreibung als Tag setzen.
+       Das Ergebnis lädst du auf dein Gerät wieder herunter und danach auf
+       <a href='https://upload.weeklypic.de/'>https://upload.weeklypic.de/</a>
+       hoch.</p>
+    <p>Disclaimer: Ja, die Bilder werden zumindest aktuell noch auf diesem
+       Server hier gespeichert und sind mit dem selbem Passwort geschützt
+       abrufbar. Ich lösche die Bilder periodisch von Hand. Für die
+       Funktionalität der Script und auch die Verfügbarkeit übernehme
+       ich weder Garantie noch Haftung.</p>
+
+    <?PHP
+      // IDEA: use cookies to store common values (Weekly-Pic-Name, Author, license)
     ?>
 
     <p>
@@ -27,14 +37,15 @@
         Datei auswählen:
         <input type="file" name="fileToUpload" id="fileToUpload" required>
         <br/>
-        Benutzername:
+        WeeklyPic-Benutzername:
         <input type="text" id="user" name="user" required>
         <br/>
-        Bild-Typ (für den Dateinamen):
-        <!-- input type="text" id="user" name="user" required -->
-
-        <br/>
-        Bildbeschreibung (optional):
+        Bild-Zeitraum (für den Dateinamen):<br>
+        <input type="radio" id="timeframe" name="timeframe" value="Woche" checked required>
+        Woche <input type="number" name="week" min="1" max="52" step="1="><br>
+        <input type="radio" id="timeframe" name="timeframe" value="Monat" required>
+        Monat <input type="number" name="month" min="1" max="12" step="1="><br/>
+        Bildbeschreibung (wird von WeeklyPic genutzt, optional):
         <input type="text" id="description" name="description">
         <br/>
         Urheber (optional):
@@ -43,9 +54,12 @@
         Lizenz (optional):
         <input type="text" id="license" name="license">
         <br/>
+        <!-- Lösche Geo-Daten -->
+        <input type="checkbox" name="nogeo" value="nogeo"> Lösche Geo-Daten aus dem Bild.<br>
+        <br>
         <input type="submit" value="Upload Image" name="submit">
       </form>
-  </p>
+    </p>
 
   </body>
 </html>
